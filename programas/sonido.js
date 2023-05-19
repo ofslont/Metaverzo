67e470999ac4fa635e19fb39ad00047c3045d2f6
Jose <ofslont@gmail.com>
2023-05-18 19:15:24 -0500

    pagina vercion incompleta

---
+++ programas/sonido.js
@@ -0,0 +1,232 @@
+let sonido = new Audio('../sonidos/do.wav');
+let sonire =new Audio('../sonidos/re.wav');
+let sonimi =new Audio('../sonidos/mi.wav');
+let sonifa =new Audio('../sonidos/fa.wav');
+let sonisol =new Audio('../sonidos/sol.wav');
+let sonila =new Audio('../sonidos/la.wav');
+let sonisi =new Audio('../sonidos/si.wav');
+let soniarp =new Audio('../sonidos/arpegio.mp3');
+let sonipia1 =new Audio('../sonidos/pianoeffect.mp3');
+let sonipia2 =new Audio('../sonidos/pianoeffect3.mp3');
+let sonipia3 =new Audio('../sonidos/pianoeffect4.mp3');
+let sonipia4 =new Audio('../sonidos/pianoeffect5.mp3');
+let sonipia5 =new Audio('../sonidos/pianoeffect6.mp3');
+let cuerda1=new Audio('../sonidos/guitarra/guitarra-afinar-cuerda-uno.mp3');
+let cuerda2=new Audio('../sonidos/guitarra/guitarra-afinar-cuerda-dos.mp3');
+let cuerda3=new Audio('../sonidos/guitarra/guitarra-afinar-cuerda-tres.mp3');
+let cuerda4=new Audio('../sonidos/guitarra/guitarra-afinar-cuerda-cuatro.mp3');
+let cuerda5=new Audio('../sonidos/guitarra/guitarra-afinar-cuerda-cinco.mp3');
+let cuerda6=new Audio('../sonidos/guitarra/guitarra-afinar-cuerda-seis.mp3');
+
+
+
+l1.addEventListener('mouseenter',()=>{
+sonido.play();
+});
+
+l2.addEventListener('mouseenter',()=>{
+    sonire.play();
+});
+
+l3.addEventListener('mouseenter',()=>{
+    sonimi.play();
+});
+
+l4.addEventListener('mouseenter',()=>{
+    sonifa.play();
+});
+
+l5.addEventListener('mouseenter',()=>{
+    sonisol.play();
+});
+
+l6.addEventListener('mouseenter',()=>{
+    sonila.play();
+});
+
+l7.addEventListener('mouseenter',()=>{
+    sonisi.play();
+});
+
+l8.addEventListener('mouseenter',()=>{
+    soniarp.play();
+});
+
+l9.addEventListener('mouseenter',()=>{
+    sonipia1.play();
+});
+
+l10.addEventListener('mouseenter',()=>{
+    sonipia2.play();
+});
+
+l11.addEventListener('mouseenter',()=>{
+    sonipia3.play();
+});
+
+l12.addEventListener('mouseenter',()=>{
+    sonipia4.play();
+});
+
+l13.addEventListener ('mouseenter',()=>{
+    sonipia5.play();
+});
+
+l14.addEventListener('mouseenter',()=>{
+    cuerda1.play();
+});
+
+l15.addEventListener('mouseenter',()=>{
+    cuerda2.play();
+});
+
+l16.addEventListener('mouseenter',()=>{
+    cuerda3.play();
+});
+
+l17.addEventListener('mouseenter',()=>{
+    cuerda4.play();
+});
+
+l18.addEventListener('mouseenter',()=>{
+    cuerda5.play();
+});
+
+l19.addEventListener('mouseenter',()=>{
+    cuerda6.play();
+});
+// _______________________________________________________________
+l20.addEventListener('mouseenter',()=>{
+    sonido.play();
+    });
+    
+    l21.addEventListener('mouseenter',()=>{
+        sonire.play();
+    });
+    
+    l22.addEventListener('mouseenter',()=>{
+        sonimi.play();
+    });
+    
+    l23.addEventListener('mouseenter',()=>{
+        sonifa.play();
+    });
+    
+    l24.addEventListener('mouseenter',()=>{
+        sonisol.play();
+    });
+    
+    l25.addEventListener('mouseenter',()=>{
+        sonila.play();
+    });
+    
+    l26.addEventListener('mouseenter',()=>{
+        sonisi.play();
+    });
+    
+    l27.addEventListener('mouseenter',()=>{
+        soniarp.play();
+    });
+    
+    l28.addEventListener('mouseenter',()=>{
+        sonipia1.play();
+    });
+    
+    l29.addEventListener('mouseenter',()=>{
+        sonipia2.play();
+    });
+    
+    l30.addEventListener('mouseenter',()=>{
+        sonipia3.play();
+    });
+    
+    l31.addEventListener('mouseenter',()=>{
+        sonipia4.play();
+    });
+    
+    l32.addEventListener ('mouseenter',()=>{
+        sonipia5.play();
+    });
+    
+    l33.addEventListener('mouseenter',()=>{
+        cuerda1.play();
+    });
+    
+    l34.addEventListener('mouseenter',()=>{
+        cuerda2.play();
+    });
+    
+    l35.addEventListener('mouseenter',()=>{
+        cuerda3.play();
+    });
+    
+    l36.addEventListener('mouseenter',()=>{
+        cuerda4.play();
+    });
+    
+    l37.addEventListener('mouseenter',()=>{
+        cuerda5.play();
+    });
+    
+    l38.addEventListener('mouseenter',()=>{
+        cuerda6.play();
+    });
+//____________________________________________________________
+l39.addEventListener('mouseenter',()=>{
+    sonido.play();
+    });
+    
+    l40.addEventListener('mouseenter',()=>{
+        sonire.play();
+    });
+    
+    l41.addEventListener('mouseenter',()=>{
+        sonimi.play();
+    });
+    
+    l42.addEventListener('mouseenter',()=>{
+        sonifa.play();
+    });
+    
+    l43.addEventListener('mouseenter',()=>{
+        sonisol.play();
+    });
+    
+    l44.addEventListener('mouseenter',()=>{
+        sonila.play();
+    });
+    
+    l45.addEventListener('mouseenter',()=>{
+        sonisi.play();
+    });
+    
+    l46.addEventListener('mouseenter',()=>{
+        soniarp.play();
+    });
+    
+    l47.addEventListener('mouseenter',()=>{
+        sonipia1.play();
+    });
+    
+    l48.addEventListener('mouseenter',()=>{
+        sonipia2.play();
+    });
+    
+    l49.addEventListener('mouseenter',()=>{
+        sonipia3.play();
+    });
+    
+    l50.addEventListener('mouseenter',()=>{
+        sonipia4.play();
+    });
+    
+    l51.addEventListener ('mouseenter',()=>{
+        sonipia5.play();
+    });
+    
+    l52.addEventListener('mouseenter',()=>{
+        cuerda1.play();
+    });
+    
+    
+
